88b4cfc1f0144bc21f6bb99d264e8399bf21df71
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-22 12:39:01 +0700

    Update header.js

--- pages/customer/header.js
+++ pages/customer/header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import CustomerPhoto from './customerPhoto';
+import CustomerPhoto from './СustomerPhoto';
 const Header = () => {
   const userId = '12351';
   const name = 'Евгений';
